f430a8693a1f7d73044c28d100377359da64d558
poornima <kannayyavaka@gmail.com>
2025-01-06 17:28:03 +0530

    expense shell

--- frontend.sh
+++ frontend.sh
@@ -55,7 +55,7 @@ VALIDATE $? "Moving to HTML directory"
 unzip /tmp/frontend.zip &>>$LOG_FILE_NAME
 VALIDATE $? "unzipping the frontend code"
 
-cp /home/ec2-user/expense-shell/expense.conf/etc/nginx/default.d/expense.conf
+cp /home/ec2-user/expense-shell/expense.conf /etc/nginx/default.d/expense.conf
 VALIDATE $? "Copied expense conf"
 
 systemctl restart nginx &>>$LOG_FILE_NAME
